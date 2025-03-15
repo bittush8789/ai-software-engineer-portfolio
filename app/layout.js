75f2924e8e7d768e3d2c52cd37d32aa79283973f
Vamsiindugu <Mytemp730@gmail.com>
2025-03-15 14:52:58 +0530

    initial commit

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+const ovo = Ovo({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const metadata = {
+  title: "Vamsi Indugu | Portfolio",
+  description: "Personal portfolio website showcasing my work and experience",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="scroll-smooth ">
+      <body
+        className={`${outfit.className} ${ovo.className} antialiased dark:bg-darkTheme dark:text-white`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
