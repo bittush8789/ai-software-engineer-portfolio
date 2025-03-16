2b7d9ca7e4e4adaf0e27039ea6ccac69d8307115
Vamsiindugu <Mytemp730@gmail.com>
2025-03-16 17:11:43 +0530

    Update Header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y:0, opacity: 1 }}
           transition={{ duration: 0.6, delay : 1}}
-          href='#contact' className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>Say Hi !<Image src={assets.arrow_icon_dark} alt='' className='w-2'/></motion.a>
+          href='#contact' className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'> Say Hi ! <Image src={assets.arrow_icon_dark} alt='' className='w-2'/></motion.a>
           <motion.a 
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y:0, opacity: 1 }}
