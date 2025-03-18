bf5f0846442a538f80446c22a5656b9f4a9e7660
Vamsiindugu <Mytemp730@gmail.com>
2025-03-18 11:57:26 +0530

    updating about section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -22,23 +22,23 @@ const About = ({isDarkMode}) => {
       whileInView={{opacity: 1, y:0}}
       transition={{ duration: 0.5, delay: 0.5}}
       
-      className='text-center text-5xl font-Ovo '>About me</motion.h2>
+      className='text-center text-5xl font-Ovo'>About me</motion.h2>
 
         <motion.div 
         initial={{ opacity: 0 }}
         whileInView={{opacity: 1}}
         transition={{ duration: 0.8}}      
         
-        className='flex w-full flex-col items-center lg:flex-row lg:items-start gap-20 my-20'>
-          <motion.div 
+        className='flex w-full flex-col items-center lg:flex-row lg:items-start gap-20 mb-9 mt-9'>
+
+          {/* <motion.div 
           initial={{ opacity: 0, scale: 0.9}}
           whileInView={{opacity: 1, scale: 1}}
           transition={{ duration: 0.6}}
                     
           className='w-64 sm:w-80 rounded-3xl max-w-none'>
             <Image src={assets.user_image} alt='user' className='w-full rounded-3xl '/>
-             
-          </motion.div>
+          </motion.div> */}
 
           <motion.div 
           initial={{ opacity: 0}}
@@ -46,11 +46,10 @@ const About = ({isDarkMode}) => {
           transition={{ duration: 0.6, delay: 0.8}}
           
           
-          className='flex-1'>
-             <p className=' mb-10 max-w-2xl font-Ovo'>
+          className='flex-1 flex flex-col items-center gap-6'>
+            <p className=' text-lg mb-10 max-w-2xl font-Ovo text-justify'>
                             A dedicated and enthusiastic programmer with a keen interest in AI and ML.🚀 <br></br>
                             I have a passion for problem-solving and pioneering first-principle thinking, driven by purpose and fueled by the challenge of finding solutions, with a good foundation in Python, Java, Javascript, AWS etc. I've developed a Machine Learning model from scratch with the commitment to making a positive impact through technology.
-
                             I'm an explorer of new tech, an avid learner, and a problem-solver at heart. When I'm not in code, you'll find me watching tech-related YouTube videos, fueling my curiosity. I'm always looking for new opportunities to learn and grow, and I'm excited to see where my journey takes me next.🚀
             </p>
             <motion.ul
@@ -68,7 +67,7 @@ const About = ({isDarkMode}) => {
                         className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover
                         hover:-translate-y-1 duration-500
                         hover:shadow-myblack dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50'
-                         key={index}>
+                          key={index}>
                             <Image src={isDarkMode ? iconDark : icon} alt={title} className='w-7 mt-3'/>
                             <h3 className='my-4 font-semibold text-gray-700 dark:text-white'>{title}</h3>
                             <p className='text-gray-600 text-sm dark:text-white/80'>{description}</p>
