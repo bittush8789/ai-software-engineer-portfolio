16996c6ad511eaf625f3328681bae8ba3b0b55a1
Vamsiindugu <Mytemp730@gmail.com>
2025-04-04 17:16:43 +0530

    update in navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
                 <Image src={isDarkMode ?  assets.sun_icon : assets.moon_icon} alt="" className='w-6'/>
             </button>
             
-            <a href="https://www.linkedin.com/in/vamsi-indugu/" target="_blank" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border-2 border-gray-500 rounded-full ml-4 font-Ovo dark:border-white/50'>Connect <Image src={ isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon} alt="" className='w-3'/></a>
+            <a href="https://www.linkedin.com/in/vamsi-indugu/" target="_blank" className=' lg:flex items-center gap-3 px-10 py-2.5 border-2 border-gray-500 rounded-full ml-4 font-Ovo dark:border-white/50'>Connect <Image src={ isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon} alt="" className='w-3'/></a>
 
             <button className='block md:hidden ml-3'onClick={openMenu}>
                 <Image src={ isDarkMode ? assets.menu_white : assets.menu_black} alt="" className='w-6'/>  
