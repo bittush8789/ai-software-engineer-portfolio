e3f1908083851b7074789b78f161bf45d9dc667f
Bittu Sharma <bittush9534@gmail.com>
2026-06-07 00:43:11 +0530

    Update contact email to bittush9534@gmail.com and make it clickable

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = ({isDarkMode}) => {
 
               <div className='w-max flex items-center gap-2 mx-auto'>
                  <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='Mail icon' className='w-6 '/>
-                 <p>vamsiindugu@gmail.com</p>
+                 <a href="mailto:bittush9534@gmail.com" className="hover:underline">bittush9534@gmail.com</a>
               </div>
         </div>
 
